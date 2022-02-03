fa92c8ca0a691e622f913576c4ac2c8f9a58009a
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-02-03 19:14:26 +0000

    testing fetch

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,54 @@
 console.log("hello");
 
+// API keys/URLs
+var testAPICall = ;
+var adzunaURL = 'https://api.adzuna.com/v1/api/jobs/gb/search/?app_id=2967701c&app_key=fe89d5a2c6186ae38284bc35150dce01&results_per_page=20&what=javascript%20developer&content-type=application/json'
+var applicationID = '2967701c'
+
+//Create Variables and link ids, add API keys
+var searchButton = ;
+var jobTypeInput = ;
+var locationInput = ;
+
+// Test Fetch Request
+fetch(adzunaURL)
+.then(function (response) {
+    if (response.ok) {
+        console.log(response);
+        response.json().then(function (data){
+            console.log(data);
+
+// Local Storage Get First
+
+// API  Calls
+
+// Set Local Storage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create Event Listeners
+
+
+
+
+
+
+
+
+                                                            //For HTML 
 
 // Collapses Nav Bar on a smaller screen
 function toggleNavbar(collapseID) {
