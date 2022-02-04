d81c3639d9aa75c3015e5107f4dffb5cb091d4b4
cee breezy <djmisscbrown@hotmail.com>
2022-02-04 13:32:36 +0000

    Added maps API to js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,29 @@
+
+//Displays Google Maps
+let map;
+let wednesburyLatLong = {lat: 52.5529, lng: -2.0221};
+const hockley = {lat: 52.49289, lng: -1.91285};
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: wednesburyLatLong,
+    zoom: 9,
+    mapId: "76d2f6e69ab59646",
+  });
+
+  //Marker
+  const marker = new google.maps.Marker({
+    position: hockley,
+    label: 'Apple',
+    map: map
+  })
+}
+
+
+
+
+
+
 console.log("hello");
 
 
@@ -16,3 +42,7 @@ function toggleNavbar(collapseID) {
         document.getElementById(collapseID).classList.toggle("py-3");
         document.getElementById(collapseID).classList.toggle("px-6");
       }
+
+
+
+
