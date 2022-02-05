ff78d5561318f602964196c1997c1ff8ac842381
cee breezy <djmisscbrown@hotmail.com>
2022-02-05 13:36:05 +0000

    Added top 5 company markers to google maps

--- assets/script.js
+++ assets/script.js
@@ -3,25 +3,67 @@ let map;
 let wednesburyLatLong = { lat: 52.5529, lng: -2.0221 };
 const softcat = { lat: 52.482575, lng: -1.91285 };
 const cisco = { lat: 51.518464, lng: -0.086249 };
+const salesForce = { lat: 51.516376, lng: -0.081206 };
+const sap = { lat: 51.449, lng: -0.445 };
+const fti = { lat: 51.5175, lng: -0.0975 };
+const sapKnutsford = { lat: 53.3031, lng: 2.3714 };
+const sapBerkshire = { lat: 51.572803, lng: -0.776339 };
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: wednesburyLatLong,
-    zoom: 10,
+    zoom: 8,
     mapId: "76d2f6e69ab59646",
-  
+    
   });
+  const marker = new google.maps.Marker({
+    position: softcat,
+    // label: "softcat",
+    map: map,
+  });
+  const marker1 = new google.maps.Marker({
+    position: cisco,
+    // label: "cisco",
+    map: map,
+  });
+
+  const marker2 = new google.maps.Marker({
+    position: salesForce,
+    // label: "Salesforce",
+    map: map,
+  });
+
+  const marker3 = new google.maps.Marker({
+    position: sap,
+    // label: "sap",
+    map: map,
+  });
+
+  const marker4 = new google.maps.Marker({
+    position: fti,
+    // label: "fti",
+    map: map,
+  });
+
+  const marker5 = new google.maps.Marker({
+    position: sapKnutsford,
+    // label: "Sap",
+    map: map,
+  });
+
+  const marker6 = new google.maps.Marker({
+    position: sapBerkshire,
+    // label: "Sap",
+    map: map,
+  });
+
+
 }
 
 
-  //Marker
-  function addMarker() {
-    const marker = new google.maps.Marker({
-      position: softcat,
-      label: "softcat",
-      map: map,
-    });
-  }
+//Marker
+  // function addMarker() {
+  //}
  
 
 
