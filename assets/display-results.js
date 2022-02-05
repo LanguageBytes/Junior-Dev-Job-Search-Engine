dee3e4b9de00a18b88d0bf9f7c5076b570c84a55
empipio <94937507+empipio@users.noreply.github.com>
2022-02-05 13:46:06 +0000

    for loop for displaying results

--- assets/display-results.js
+++ assets/display-results.js
@@ -16,3 +16,36 @@
 // Date posted: results[i].date
 // Job description: results[i].jobDescription
 // Apply here/job link: results[i].jobUrl
+
+//for loop
+//create card
+//append each bit of results for list page
+
+for (var i = 0; i < results.length; i++) {
+  var resultArea = document.getElementById("results");
+  var resultCard = document.createElement("div");
+  resultCard.classList.add("card-body");
+  resultArea.append(resultCard);
+
+  var jobTypeEl = document.createElement("h4");
+  jobTypeEl.textContent = results[i].jobTitle;
+  resultCard.append(jobTypeEl);
+  //add href attribute here to link to further job details page
+
+  var locationEl = document.createElement("div");
+  locationEl.textContent = "Location: " + results[i].locationName;
+  resultCard.append(locationEl);
+
+  var employerEl = document.createElement("div");
+  employerEl.textContent = "Employer: " + results[i].employerName;
+  resultCard.append(employerEl);
+
+  var salaryEl = document.createElement("div");
+  salaryEl.textContent =
+    "Salary: " + results[i].minimumSalary + "-" + results[i].maximumSalary;
+  resultCard.append(salaryEl);
+
+  var closingEl = document.createElement("div");
+  closingEl.textContent = results[i].expirationDate;
+  resultCard.append(expirationDate);
+}
