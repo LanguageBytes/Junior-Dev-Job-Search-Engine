3696ef58a8e88f660c8bcd8d4a5d27783312b905
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-02-07 13:23:14 +0000

    JS make search function linked with the search box on results page

--- assets/display-results.js
+++ assets/display-results.js
@@ -19,9 +19,7 @@
 
 //var location = document.location.search.split("=").pop();
 
-
-
-//  HTML Script
+                                                     //  HTML Script
 
 // Collapses Nav Bar on a smaller screen
 function toggleNavbar(collapseID) {
@@ -38,7 +36,7 @@ function toggleNavbar(collapseID) {
     document.getElementById(collapseID).classList.toggle("px-6");
   }
   
-
+                                        //   Get Search from Previous Page
 function getJobs() {
   var apiKey = "c8be0d68-4d2d-4751-943b-da6b6d189413";
   var encodedKey = btoa(`${apiKey}:`);
@@ -112,7 +110,7 @@ function getJobs() {
 getJobs();
 
 
-// Make Search SideBar and Filters
+                                                 // Make Search with Filters
 
 var locationInput = document.getElementById ("location-input")
 var searchButton = document.getElementById ("search-button")
@@ -120,35 +118,78 @@ var results = document.getElementById("results")
 
 // Search API
 var makeSearch = function (event) {
-  event.prevent.default()
+  event.preventDefault()
     // This will take the user input from the query string for us to use
-var userLocationInput = window.location.search.slice(3);
+var userLocationInput = locationInput.value.trim();
 console.log(userLocationInput)
  
  // REED Variables
  var apiKey = "c8be0d68-4d2d-4751-943b-da6b6d189413";
  var encodedKey = btoa(`${apiKey}:`);
  var authHeader = `Basic ${encodedKey}`;
- var keywords = ["junior%20developer", ""];
+ var keywords = ["junior%20developer"];
  var corsAnywhereLink = "https://radiant-stream-08604.herokuapp.com/";
  var queryURL =
  corsAnywhereLink +
  "https://www.reed.co.uk/api/1.0/search?keywords=" +
  keywords + "&locationName=" + userLocationInput + "&distanceFromLocation=" + 10;
 
- fetch(queryURL, {
-   headers: {
-     Authorization: authHeader,
-   },
- })
-   .then(jsonResponse)
-   .then(returnJobs);
+// REMOVE PREVIOUSLY SEARCHED RESULTS
 
-   function jsonResponse(res) {
-     return res.json();
-   }
-   
-   function returnJobs(results) {
-     console.log(results);
-   }
+  // Fetch Request
+  fetch(queryURL, {
+    headers: {
+      Authorization: authHeader,
+    },
+  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (jobs) {
+      console.log(jobs);
+      console.log(jobs.results[0].employerName);
+
+      for (var i = 0; i < jobs.results.length; i++) {
+        var resultArea = document.getElementById("results");
+        var resultCard = document.createElement("div");
+        resultCard.classList.add("card-body");
+        resultArea.append(resultCard);
+
+        var jobTypeEl = document.createElement("h4");
+        jobTypeEl.textContent = jobs.results[i].jobTitle;
+        //link here for next page if click on job title
+        // var jobLinkEl = document.createElement("a");
+        // jobLinkEl.setAttribute("src", "result page");
+        // jobTypeEl.append(jobLinkEl);
+        resultCard.append(jobTypeEl);
+
+        var locationEl = document.createElement("div");
+        locationEl.textContent = "Location: " + jobs.results[i].locationName;
+        resultCard.append(locationEl);
+
+        var employerEl = document.createElement("div");
+        employerEl.textContent = "Employer: " + jobs.results[i].employerName;
+        resultCard.append(employerEl);
+
+        var salaryEl = document.createElement("div");
+        salaryEl.textContent =
+          "Salary: " +
+          jobs.results[i].minimumSalary +
+          "-" +
+          jobs.results[i].maximumSalary;
+        resultCard.append(salaryEl);
+
+        var closingEl = document.createElement("div");
+        closingEl.textContent =
+          "Closing date: " + jobs.results[i].expirationDate;
+        resultCard.append(closingEl);
+
+        var cardBreak = document.createElement("br");
+        resultCard.append(cardBreak);
+      }
+    });
 };
+
+
+//Event listener for search button
+searchButton.addEventListener("click", makeSearch);
