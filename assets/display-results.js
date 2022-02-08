5780e871b0576f74a50e374f6af9b36bdbc65986
empipio <94937507+empipio@users.noreply.github.com>
2022-02-08 18:20:51 +0000

    query string added to link on results page

--- assets/display-results.js
+++ assets/display-results.js
@@ -19,7 +19,7 @@
 
 //var location = document.location.search.split("=").pop();
 
-                                                          //  HTML Script
+//  HTML Script
 
 // Collapses Nav Bar on a smaller screen
 function toggleNavbar(collapseID) {
@@ -36,36 +36,32 @@ function toggleNavbar(collapseID) {
   document.getElementById(collapseID).classList.toggle("px-6");
 }
 
-
-                                                            //HTML Variables
+//HTML Variables
 
 var searchButton = document.getElementById("search-button");
 var results = document.getElementById("results");
 var stored = document.getElementById("history");
 
-
-                                              // Redirected from Homepage Search
+// Redirected from Homepage Search
 
 // Get Search Input from the Previous Page
 function getJobs() {
-
-  
-  //Takes the homepage search results from the querystring 
+  //Takes the homepage search results from the querystring
   var location = document.location.search.split("=").pop();
 
   // Reed Variables
   var apiKey = "c8be0d68-4d2d-4751-943b-da6b6d189413";
   var encodedKey = btoa(`${apiKey}:`);
   var authHeader = `Basic ${encodedKey}`;
-  var keywords = "junior%20developer"; 
+  var keywords = "junior%20developer";
   var corsAnywhereLink = "https://radiant-stream-08604.herokuapp.com/";
 
   var queryURL =
     corsAnywhereLink +
     "https://www.reed.co.uk/api/1.0/search?keywords=" +
-    keywords + 
+    keywords +
     "&locationName=" +
-    location + 
+    location +
     "&distanceFromLocation=" +
     10;
 
@@ -76,28 +72,28 @@ function getJobs() {
     },
   })
     .then(function (res) {
-      console.log (res);
+      console.log(res);
       return res.json();
-      
     })
     .then(function (jobs) {
       console.log(jobs);
       console.log(jobs.results[0].employerName);
 
-
       // Creates cards in the HTML for each job
       for (var i = 0; i < jobs.results.length; i++) {
-       
         var resultArea = document.getElementById("results");
-        
+
         var resultCard = document.createElement("div");
-        resultCard.setAttribute("id", "discardLater")
+        resultCard.setAttribute("id", "discardLater");
         resultCard.classList.add("card-body");
         resultArea.append(resultCard);
 
         var jobTypeEl = document.createElement("a");
         jobTypeEl.textContent = jobs.results[i].jobTitle;
-        jobTypeEl.setAttribute("href", "./description-page.html");
+        var jobId = jobs.results[i].jobId;
+        var descriptionLink =
+          "./description-page.html?q=" + location + "&jobid=" + jobId;
+        jobTypeEl.setAttribute("href", descriptionLink);
         resultCard.append(jobTypeEl);
 
         var locationEl = document.createElement("div");
@@ -122,9 +118,8 @@ function getJobs() {
         resultCard.append(closingEl);
 
         var readMoreEl = document.createElement("button");
-        readMoreEl.textContent =
-          "Read Description";
-        readMoreEl.classList.add('readMore')
+        readMoreEl.textContent = "Read Description";
+        readMoreEl.classList.add("readMore");
         resultCard.append(readMoreEl);
 
         var cardBreak = document.createElement("br");
@@ -136,59 +131,57 @@ function getJobs() {
     });
 }
 
-getJobs()
+getJobs();
 
+// Search on Page with Filters
+newSearch = function (event) {
+  event.preventDefault();
 
-
-                                            // Search on Page with Filters                                      
-newSearch = function(event) {
-  event.preventDefault()
-  
-
-//This will get rid of the previous searches by removing the 'discardLater' id added to resultCard (child of #results)
+  //This will get rid of the previous searches by removing the 'discardLater' id added to resultCard (child of #results)
   function removeAllChildNodes(parent) {
     while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+      parent.removeChild(parent.firstChild);
     }
   }
-  var previous = document.querySelector('#results');
+  var previous = document.querySelector("#results");
   removeAllChildNodes(previous);
- 
 
-//This process is the same as getJobs but with filters added to it and local storage added
+  //This process is the same as getJobs but with filters added to it and local storage added
 
   // Reed Variables
   var apiKey = "c8be0d68-4d2d-4751-943b-da6b6d189413";
   var encodedKey = btoa(`${apiKey}:`);
   var authHeader = `Basic ${encodedKey}`;
-  var keywords = ["junior%20developer%20", language]; 
+  var keywords = ["junior%20developer%20", language];
   var corsAnywhereLink = "https://radiant-stream-08604.herokuapp.com/";
 
   //FILTERS
-    var language = document.getElementById("language-input").value;
-    var locationInput = document.getElementById("location-input");
-    var minimumSalary = document.getElementById("minimum-salary-input").value;
-    var maximumSalary = document.getElementById("maximum-salary-input").value;
-    var userLocationInput = locationInput.value.trim();
-    console.log(userLocationInput);
+  var language = document.getElementById("language-input").value;
+  var locationInput = document.getElementById("location-input");
+  var minimumSalary = document.getElementById("minimum-salary-input").value;
+  var maximumSalary = document.getElementById("maximum-salary-input").value;
+  var userLocationInput = locationInput.value.trim();
+  console.log(userLocationInput);
 
- // URL
+  // URL
   var queryURL =
-  corsAnywhereLink +
-  "https://www.reed.co.uk/api/1.0/search?keywords=" +
-  keywords +
-  "&locationName=" +
-  userLocationInput + 
-  "&distanceFromLocation=" +
-  20 + "&minimumSalary=" + minimumSalary + "&maximumSalary=" + maximumSalary;
+    corsAnywhereLink +
+    "https://www.reed.co.uk/api/1.0/search?keywords=" +
+    keywords +
+    "&locationName=" +
+    userLocationInput +
+    "&distanceFromLocation=" +
+    20 +
+    "&minimumSalary=" +
+    minimumSalary +
+    "&maximumSalary=" +
+    maximumSalary;
 
-
-// if the user has not entered a search term
-if (!userLocationInput) {
-  console.error("You need a search input value, you silly banana!");
-  return;
-}
-
+  // if the user has not entered a search term
+  if (!userLocationInput) {
+    console.error("You need a search input value, you silly banana!");
+    return;
+  }
 
   // Fetch Request
   fetch(queryURL, {
@@ -197,23 +190,20 @@ if (!userLocationInput) {
     },
   })
     .then(function (res) {
-      console.log (res);
+      console.log(res);
       return res.json();
-      
     })
     .then(function (jobs) {
       console.log(jobs);
       console.log(jobs.results[0].employerName);
 
-  
       //Creates cards in the HTML for each job
       for (var i = 0; i < jobs.results.length; i++) {
         var resultArea = document.getElementById("results");
         var resultCard = document.createElement("div");
-        resultCard.setAttribute("id", "discardLater")
+        resultCard.setAttribute("id", "discardLater");
         resultCard.classList.add("card-body");
         resultArea.append(resultCard);
-          
 
         var jobTypeEl = document.createElement("h4");
         jobTypeEl.textContent = jobs.results[i].jobTitle;
@@ -245,9 +235,8 @@ if (!userLocationInput) {
         resultCard.append(closingEl);
 
         var readMoreEl = document.createElement("button");
-        readMoreEl.textContent =
-          "Read Description";
-        readMoreEl.classList.add('readMore')
+        readMoreEl.textContent = "Read Description";
+        readMoreEl.classList.add("readMore");
         resultCard.append(readMoreEl);
 
         var cardBreak = document.createElement("br");
@@ -257,13 +246,7 @@ if (!userLocationInput) {
         resultCard.append(cardBreak);
       }
     });
-  }
+};
 
- 
 //Event listener for search button
 searchButton.addEventListener("click", newSearch);
-
-
-
-
-
